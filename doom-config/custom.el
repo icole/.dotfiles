1403e0937ac21b180e960ac2cd9ecbb0c3fbf47e
Ian Cole <imcole@pm.me>
2022-02-22 09:30:37 -0800

    Add doom config

---
+++ doom-config/custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#1e1d2f" "#f28fad" "#abe9b3" "#fae3b0" "#6272a4" "#bd93f9" "#89dceb" "#d9e0ee"])
+ '(custom-safe-themes
+   '("234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "970f1a4c7349ba44a4d06a80cc88dd79961c054f37865c93c9cfec40806ea001" "e990fd36b40108405468d7d338feff98420ab967d824115a9a92668bad949af9" default))
+ '(delete-selection-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
