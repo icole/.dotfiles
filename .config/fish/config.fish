cbc8b8d46d231c69eff17285fa92e5fb2dc18e77
Ian Cole <imcole@pm.me>
2022-02-23 20:18:57 -0800

    Add dotnet tools to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
 end
 
-set -x PATH $PATH ~/.emacs.d/bin ~/.local/bin ./node_modules/.bin
+set -x PATH $PATH ~/.emacs.d/bin ~/.local/bin ./node_modules/.bin ~/.dotnet/tools
 
 starship init fish | source
