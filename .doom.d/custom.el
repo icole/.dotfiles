ce556feb998de7c2a91f7629a08ce85d10341a80
Ian Cole <icole@auditboard.com>
2022-05-11 12:25:47 -0700

    Move doom config again

---
+++ .doom.d/custom.el
@@ -0,0 +1,48 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#1e1d2f" "#f28fad" "#abe9b3" "#fae3b0" "#6272a4" "#bd93f9" "#89dceb" "#d9e0ee"])
+ '(custom-safe-themes
+   '("97db542a8a1731ef44b60bc97406c1eb7ed4528b0d7296997cbb53969df852d6" "353ffc8e6b53a91ac87b7e86bebc6796877a0b76ddfc15793e4d7880976132ae" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "970f1a4c7349ba44a4d06a80cc88dd79961c054f37865c93c9cfec40806ea001" "e990fd36b40108405468d7d338feff98420ab967d824115a9a92668bad949af9" default))
+ '(delete-selection-mode nil)
+ '(exwm-floating-border-color "#252630")
+ '(fci-rule-color "#9099c0")
+ '(highlight-tail-colors ((("#222e36") . 0) (("#29313b") . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#414868" "#b4f9f8"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#414868" "#73daca"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#414868" "#8189af"))
+ '(objed-cursor-color "#f7768e")
+ '(pdf-view-midnight-colors (cons "#a9b1d6" "#1a1b26"))
+ '(rustic-ansi-faces
+   ["#1a1b26" "#f7768e" "#73daca" "#e0af68" "#7aa2f7" "#bb9af7" "#b4f9f8" "#a9b1d6"])
+ '(vc-annotate-background "#1a1b26")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#73daca")
+    (cons 40 "#97cba9")
+    (cons 60 "#bbbd88")
+    (cons 80 "#e0af68")
+    (cons 100 "#eaa966")
+    (cons 120 "#f4a365")
+    (cons 140 "#ff9e64")
+    (cons 160 "#e89c94")
+    (cons 180 "#d19bc5")
+    (cons 200 "#bb9af7")
+    (cons 220 "#cf8ed4")
+    (cons 240 "#e382b1")
+    (cons 260 "#f7768e")
+    (cons 280 "#d97a96")
+    (cons 300 "#bc7f9e")
+    (cons 320 "#9e84a6")
+    (cons 340 "#9099c0")
+    (cons 360 "#9099c0")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
