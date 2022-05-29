3ee4c3dcdd6e2475f17ab42171c948b9cfa328ca
Ian Cole <ian@colecoding.com>
2022-05-29 14:54:26 -0700

    Switch back to blueish posh theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,4 +3,4 @@ end
 
 set -U fish_user_paths ~/.local/bin ./node_modules/.bin $fish_user_paths
 
-oh-my-posh init fish --config ~/.poshthemes/nord.omp.json | source
+oh-my-posh init fish --config ~/.poshthemes/blueish.omp.json | source
