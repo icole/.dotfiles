ed284b7b45a4d7fec7d1233e49325048ac7cf222
Ian Cole <icole@auditboard.com>
2023-02-20 20:56:40 -0800

    Add docker fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,3 +6,5 @@ set -U fish_user_paths ~/.local/bin ./node_modules/.bin $fish_user_paths
 oh-my-posh init fish --config ~/.poshthemes/blueish.omp.json | source
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+source /Users/icole/.docker/init-fish.sh || true # Added by Docker Desktop
